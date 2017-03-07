4c3cd3fcd5eafa055fcc0d317a9d7f4339b86463
slowik_p <p.slowik@synthrone.com>
2017-03-07 08:44:41 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My first angular2 app';
+  title = 'My first angular2 app!!!';
 }
